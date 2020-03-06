09eea931f4aa80dd485806a88a4ba72ffeb81716
anantakit <frame.zz@hotmail.com>
2020-03-06 10:16:43 +0700

    fix logic

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -20,14 +20,6 @@ router.post("/transactions/:userId", async (req, res) => {
 
     //console.log(user_result);
 
-    const newTransactionModel = new transactions({
-      uid: user_result._id,
-      ...req.body
-    });
-    console.log("newTransactionModel", newTransactionModel);
-    await newTransactionModel.save();
-    console.log("Insert Transaction Success");
-
     // find financial
     let financial_result = await financialInfo.find();
 
@@ -35,7 +27,11 @@ router.post("/transactions/:userId", async (req, res) => {
       console.log("Transaction Type Loan: ");
       await user.updateOne(
         { _id: user_result._id },
-        { $inc: { totalLoan: req.body.amount } }
+        {
+          $inc: { totalLoan: req.body.amount },
+          remainingAmount:
+            user_result.totalLoan + req.body.amount - user_result.paidAmount
+        }
       );
       console.log("********Update User Success");
 
@@ -44,13 +40,14 @@ router.post("/transactions/:userId", async (req, res) => {
         {
           $inc: { totalDebt: req.body.amount },
           totalRemainingAmount:
-            financial_result[0].totalDebt -
-            (financial_result[0].totalPaidAmount + req.body.amount)
+            financial_result[0].totalDebt +
+            req.body.amount -
+            financial_result[0].totalPaidAmount
         }
       );
       console.log("********Update Financial Success");
-    } else if ("payment") {
-      console.log("Transaction Type payment: ", financial_result[0]);
+    } else if (req.body.type === "payment") {
+      console.log("Transaction Type payment: ");
       await user.updateOne(
         { _id: user_result._id },
         {
@@ -78,42 +75,24 @@ router.post("/transactions/:userId", async (req, res) => {
       console.log("********Update Financial Success");
     }
 
-    res.send({user : await user.findById(req.params.userId) , financial : await financialInfo.find(), transaction : newTransactionModel , reqBody : req.body});
+    const newTransactionModel = new transactions({
+      uid: user_result._id,
+      ...req.body
+    });
+
+    console.log("newTransactionModel", newTransactionModel);
+    await newTransactionModel.save();
+    console.log("Insert Transaction Success");
+
+    res.send({
+      user: await user.findById(req.params.userId),
+      financial: await financialInfo.find(),
+      transaction: newTransactionModel,
+      reqBody: req.body
+    });
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
 module.exports = router;
-
-// else {
-//   console.log("Transaction Type payment: ");
-//   await user.updateOne(
-//     { _id: user_result._id },
-//     {
-//       $inc: {
-//         paidAmount: req.body.amount
-//       }
-//     },
-//     {
-//       $set: {
-//         remainingAmount:
-//           totalLoan - (user_result.paidAmount + req.body.amount)
-//       }
-//     }
-//   );
-
-//   console.log("********Update User Success");
-
-//   await financialInfo.updateOne(
-//     { _id: financial_result[0]._id },
-//     { $inc: { totalPaidAmount: amount } },
-//     {
-//       $set: {
-//         totalRemainingAmount:
-//           financial_result.totalDebt - (totalPaidAmount + req.body.amount)
-//       }
-//     }
-//   );
-//   console.log("********Update Financial Success");
-// }
